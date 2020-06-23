4c3ea1932e755438e8d3e71dce1d9a01d386c8e3
jdaeumler <jenniferdaeumler@gmail.com>
2020-06-23 18:02:06 -0500

    Character pics

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-    console.log( "ready!" );
+var gifApiKey = "3jMvNzOuh5eBAmKGoigDH6ZFcSFVgdoG";
+var gifURL= "https://api.giphy.com/v1/gifs/translate?api_key="+ gifApiKey + "&s=" + characterName;
+
+// $.ajax({
+//     url: gifURL,
+//     method: "GET",
+//   }).then(function(response){
+// //   console.log(response.data.images.original.url);
+// // characterName = value of click
+// var imageUrl = response.data.images.original.url;
+// })
+
+
 });
